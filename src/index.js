87870ea16fe56ca0a9490f440cd1e92cc980bdff
Meebky <jesus-roman15@hotmail.com>
2026-06-08 22:13:46 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ conectarDB();
 const app = express();
 
 // ── Middlewares ──────────────────────────────────────────
-app.use(cors());
+app.use(cors({ origin: "*, methods: [GET,POST,PUT,DELETE,OPTIONS], allowedHeaders: [Content-Type,Authorization] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
